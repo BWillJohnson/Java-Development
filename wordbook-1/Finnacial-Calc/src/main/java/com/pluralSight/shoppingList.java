c130dda62406dce766da9b9940fb63a38b1023fe
Brandon <brandonjohnson.bj880@gmail.com>
2024-09-25 13:34:33 -0400

    intial commit

---
+++ wordbook-1/Finnacial-Calc/src/main/java/com/pluralSight/shoppingList.java
@@ -0,0 +1,20 @@
+package com.pluralSight;
+
+public class shoppingList
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Pancake-mix");
+        System.out.println("Blueberries");
+        System.out.println("Waffles");
+        System.out.println("Grapes");
+        System.out.println("Sencha-Tea");
+        System.out.println("Salad");
+        System.out.println("Steak");
+        System.out.println("Ribs");
+        System.out.println("Potatoes");
+        System.out.println("Gomu-Gomu-Fruit");
+        System.out.println("My-Shopping-List");
+    }
+
+}
