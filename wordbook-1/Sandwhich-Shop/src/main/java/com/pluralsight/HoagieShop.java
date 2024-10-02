fe56887f1d5c8f98b6096a39d5a3708d59f9bd4d
Brandon <brandonjohnson.bj880@gmail.com>
2024-10-02 13:06:24 -0400

    Updated, completed

--- wordbook-1/Sandwhich-Shop/src/main/java/com/pluralsight/HoagieShop.java
+++ wordbook-1/Sandwhich-Shop/src/main/java/com/pluralsight/HoagieShop.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HoagieShop {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Hey there, Whats your age to recieve a hoagie discount.");
+        System.out.println("Hey there, Whats your age to received a hoagie discount.");
         System.out.println("Enter age:     ");
         int age = input.nextInt();
 
@@ -14,17 +14,32 @@ public class HoagieShop {
         } else {
             System.out.println("Great you have received a 20% discount!");
         }
-        calculateFinalPrice(age, input);
-    }
 
-    public static double calculateFinalPrice(int age, Scanner myScan) {
 
         System.out.println("What size hoagie would you like?");
-        System.out.println(" We have a reg size [1] hoagie for $5.45, and a [2] Large hoagie for $8.95:  " );
+        System.out.println(" We have a reg size [1] hoagie for $5.45, and a [2] Large hoagie for $8.95");
+        System.out.println("Enter your hoagieSize: ");
+        int hoagieSize = input.nextInt();
 
-        double basePrice1 = 5.45 * 0;
-        double basePrice = 8.95 * 0;
+        double basePrice;
+        if (hoagieSize == 1) {
+            basePrice = 5.45;
+        } else if (hoagieSize == 2) {
+            basePrice = 5.45;
+        } else {
+            System.out.println("Error hoagie size");
+            return;
+        }
 
+
+        double discountPercentage;
+        if (age <= 17) {
+            discountPercentage = 0.1;
+        } else {
+            discountPercentage = 0.2;
+        }
+        double finalPrice = basePrice * (1 - discountPercentage);
+        System.out.println(finalPrice);
 
     }
 }
