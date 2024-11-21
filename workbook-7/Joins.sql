cb0acca888aa8619c69f741b4bc6e2bb5eaca02c
Brandon <brandonjohnson.bj880@gmail.com>
2024-11-20 19:22:13 -0500

    Alomst complet with joins Excersise have

--- workbook-7/Joins.sql
+++ workbook-7/Joins.sql
@@ -1,6 +1,20 @@
--- Question 1:
+-- Question 1: 
+SELECT p.ProductID,p.ProductName,p.UnitPrice, c.CategoryName
+From products p
+INNER JOIN categories c
+                   ON p.CategoryID = c.CategoryID
+                   ORDER  BY c.CategoryName, p.ProductName;
 -- Question 2:
+SELECT p.ProductID, p.ProductName, p.UnitPrice, s.ContactName
+FROM products p
+RIGHT JOIN suppliers s
+					ON p.ProductName = s.ContactName
+                    WHERE p.UnitPrice < 75
+                    ORDER BY p.ProductName;
 -- Question 3:
+
 -- Question 4:
+
 -- Question 5:
+
 -- Question 6:
