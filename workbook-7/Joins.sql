9a0477322595aeefeea3204901428f8159019c45
Brandon <brandonjohnson.bj880@gmail.com>
2024-11-21 10:01:21 -0500

    Completed Joins excersise.

--- workbook-7/Joins.sql
+++ workbook-7/Joins.sql
@@ -1,20 +1,33 @@
 -- Question 1: 
-SELECT p.ProductID,p.ProductName,p.UnitPrice, c.CategoryName
+SELECT p.ProductID,p.ProductName, p.UnitPrice, c.CategoryName
 From products p
 INNER JOIN categories c
                    ON p.CategoryID = c.CategoryID
                    ORDER  BY c.CategoryName, p.ProductName;
 -- Question 2:
-SELECT p.ProductID, p.ProductName, p.UnitPrice, s.ContactName
+SELECT p.ProductID, p.ProductName, p.UnitPrice, s.CompanyName AS SupplierName
 FROM products p
 RIGHT JOIN suppliers s
-					ON p.ProductName = s.ContactName
-                    WHERE p.UnitPrice < 75
+					ON p.ProductID = s.SupplierID
+                    WHERE p.UnitPrice > 75
                     ORDER BY p.ProductName;
 -- Question 3:
-
+SELECT p.ProductID, p.ProductName, p.UnitPrice, c.CategoryName, s.CompanyName AS SupplierName 
+FROM products p
+				INNER JOIN categories c
+				ON p.CategoryID = c.CategoryID
+				INNER JOIN suppliers s
+                ON p.SupplierID = s.SupplierID
+                ORDER BY p.ProductName;
 -- Question 4:
-
+SELECT p.ProductName, p.UnitPrice, c.Description AS Category
+FROM products p
+			INNER JOIN categories c
+            ORDER BY p.UnitPrice DESC;
+            
 -- Question 5:
+SELECT o.OrderID, o.shipName, o.ShipAddress,
+FROM Orders o;
+
 
 -- Question 6:
